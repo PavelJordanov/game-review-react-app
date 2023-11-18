b8cc0b38ac0e83bb37428c26bf42f74c661845bd
Pavel Jordanov <paj4@sfu.ca>
2023-11-17 23:21:23 -0800

    Second Draft

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 function ListGroup() {
-    return <h1>List Group</h1>
+    let items = [
+        'New York',
+        'Vancouver',
+        'Toronto',
+        'Sofia',
+        'London'
+    ];
+
+    // hook function: allows to tap into built in features in react
+    // using the state hook we can tell react that this component will
+    // have data or a state change over time
+    
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    return (
+        <>
+            <h1>List</h1>
+            {items.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+                {items.map((item, index) => <li 
+                className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} 
+                key={item} 
+                onClick={() => {setSelectedIndex(index)}}>
+                    {item}
+                </li>)}
+            </ul>
+        </>
+    );
 }
 
 export default ListGroup;
