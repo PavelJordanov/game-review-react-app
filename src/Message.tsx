3a518984732f7004ceb5262301fb468dbccde5f1
Pavel Jordanov <paj4@sfu.ca>
2023-11-17 21:28:07 -0800

    First draft

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing is standard react type style
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Pavel';
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;
